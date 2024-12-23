483fb1966f572224711db41732304b64d1c08196
Baqer Alshakhs <balshakhs1@gmail.com>
2024-12-23 14:45:00 +0000

    Update install_tesseract.sh

--- install_tesseract.sh
+++ install_tesseract.sh
@@ -5,8 +5,9 @@ mkdir -p $HOME/tesseract
 cd $HOME/tesseract
 
 # Download and extract Tesseract binary for Linux
-wget https://github.com/tesseract-ocr/tesseract/releases/download/5.0.0/tesseract-5.0.0-linux-x86_64.tar.gz
-tar -xvzf tesseract-5.0.0-linux-x86_64.tar.gz --strip-components=1
+wget https://github.com/tesseract-ocr/tesseract/releases/download/5.3.0/tesseract-5.3.0-linux-x86_64.tar.gz
+tar -xvzf tesseract-5.3.0-linux-x86_64.tar.gz -C /usr/local/
+
 
 # Add Tesseract binary to the PATH for immediate access
 export PATH=$HOME/tesseract/bin:$PATH
